f189c8241327a3a68c9885e30a9fc4e723c2d03d
Rafael Sanabria Cruz <rafasanabria1@gmail.com>
2023-04-27 14:07:38 +0200

    Apply to canva element styles from const file

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import { observer } from "mobx-react";
 import Box from "../components/Box";
+import { SIZES } from "../utils/const";
 
 function Canvas({ store }) {
   return (
-    <div className="canva">
+    <div className="canva" style={{width: SIZES.CANVA.WIDTH, height: SIZES.CANVA.HEIGHT}}>
       {store.boxes.map((box, index) => (
         <Box
           id={box.id}
